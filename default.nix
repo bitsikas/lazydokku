cff46b98fe8c08784fab1acfe072fc0980658b58
Kostas Papakonstantinou <kostas@bitsikas.dev>
2022-02-04 19:07:30 +0200

    Fix requirements install for py_cui in nix package

--- default.nix
+++ default.nix
@@ -2,4 +2,11 @@
 
 pkgs.poetry2nix.mkPoetryApplication {
   projectDir = ./.;
+  overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
+      py-cui = super.py-cui.overridePythonAttrs (attrs: {
+        patches = [
+	   ./py_cui_setup.patch
+        ];
+      });
+    });
 }
