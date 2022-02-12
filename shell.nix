7a57dae21484e00e61e9fb04aacf3e5dbdbc2408
Kostas Papakonstantinou <kostas@bitsikas.dev>
2022-02-12 13:06:35 +0200

    use poetry2nix in shell as well

--- shell.nix
+++ shell.nix
@@ -1,11 +1,16 @@
 { pkgs ? import <nixpkgs> {} }:
-
-pkgs.mkShell {
-
-
-  buildInputs = [
-    pkgs.python3
-    pkgs.poetry
-  ];
-
-}
+let
+  lazydokku = pkgs.poetry2nix.mkPoetryEnv {
+    projectDir = ./.;
+    editablePackageSources = {
+      my-app = ./lazydokku;
+    };
+    overrides = pkgs.poetry2nix.overrides.withDefaults (self: super: {
+      py-cui = super.py-cui.overridePythonAttrs (attrs: {
+        patches = [
+          ./py_cui_setup.patch
+        ];
+      });
+    });
+  };
+in lazydokku.env
